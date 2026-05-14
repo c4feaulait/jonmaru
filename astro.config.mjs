5b8f30aaa09bfc3457a601cb08a8290859f12be9
c4feaulait <romarin.radiation@gmail.com>
2026-05-14 23:17:55 +0900

    breakpoint、mixinの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // vite: {
+    //     css: {
+    //         preprocessorOptions: {
+    //             scss: {
+    //             additionalData: `
+    //             @use "global/_breakpoints.scss" as mixin;
+    //             `,
+    //             },
+    //         },
+    //     },
+    // },
+});
